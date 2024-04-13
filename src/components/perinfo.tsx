498366fb0ff15c5bb7874a222b297d3eb5010b7e
NishantGupt786 <nishantgupta2325@gmail.com>
2024-04-13 15:37:47 +0530

    more in acc info

--- src/components/perinfo.tsx
+++ src/components/perinfo.tsx
@@ -5,9 +5,12 @@ import { Pencil } from "lucide-react";
 
 export default function PerInfo() {
   //   const [details, setDetails] = useState(null);
-  const [pregnant, setPregnant] = useState(false);
-  const [hadChildBefore, setHadChildBefore] = useState(false);
-  const [numberOfChildren, setNumberOfChildren] = useState(0);
+  const [pregnant, setPregnant] = useState("no");
+  const [pregInput, togglePregInput] = useState(false);
+  const [child, setChild] = useState("no");
+  const [childInput, toggleChildInput] = useState(false);
+  const [num, setNum] = useState("0");
+  const [numInput, toggleNumInput] = useState(false);
   //   useEffect(() => {
   //     const access_token: string | null = localStorage.getItem("access_token");
   //     async function getDetails() {
@@ -19,74 +22,68 @@ export default function PerInfo() {
   //       setDetails(response.data)
   //     }
   //   }, []);
-  const toggleEditMode = (fieldName: string) => {
-    switch (fieldName) {
-      case "pregnant":
-        setPregnant(!pregnant);
-        break;
-      case "hadChildBefore":
-        setHadChildBefore(!hadChildBefore);
-        break;
-      case "numberOfChildren":
-        setNumberOfChildren(0); // Reset number of children when editing
-        break;
-      default:
-        break;
-    }
-  };
 
-  const handleOptionChange = (fieldName: string, value: string) => {
-    switch (fieldName) {
-      case "pregnant":
-        setPregnant(value === "Yes");
-        break;
-      case "hadChildBefore":
-        setHadChildBefore(value === "Yes");
-        break;
-      default:
-        break;
-    }
-  };
   return (
     <>
       <div className="flex flex-col text-black mt-6">
         <div className="flex flex-col border-b-[1px] border-[#00000033] w-[45%] pb-4">
           <p className="font-semibold pb-1">Are you currently pregnant?</p>
-          {pregnant ? (
-            <select
-              value={pregnant ? "Yes" : "No"}
-              onChange={(e) => handleOptionChange("pregnant", e.target.value)}
-              onBlur={() => toggleEditMode("pregnant")}
-            >
-              <option value="Yes">Yes</option>
-              <option value="No">No</option>
-            </select>
-          ) : (
-            <div className="flex justify-between">
-              <p className="text-[#000000CC] font-normal">
-                {pregnant ? "Yes" : "No"}
-              </p>
-              <Pencil
-                color="#0C21C1B3"
-                size={20}
-                className="cursor-pointer"
-                onClick={() => toggleEditMode("pregnant")}
+          <div className="flex justify-between">
+            {pregInput ? (
+              <input
+                value={pregnant}
+                onChange={(e) => setPregnant(e.target.value)}
+                onBlur={() => togglePregInput(false)}
               />
-            </div>
-          )}
+            ) : (
+              <p className="text-[#000000CC] font-normal">{pregnant}</p>
+            )}
+            <Pencil
+              color="#0C21C1B3"
+              size={20}
+              className="cursor-pointer"
+              onClick={() => togglePregInput(true)}
+            />
+          </div>
         </div>
         <div className="flex flex-col border-b-[1px] border-[#00000033] w-[45%] py-4">
           <p className="font-semibold pb-1">Have you had a child before?</p>
           <div className="flex justify-between">
-            <p className="text-[#000000CC] font-normal">No</p>
-            <Pencil color="#0C21C1B3" size={20} className="cursor-pointer" />
+            {childInput ? (
+              <input
+                value={child}
+                onChange={(e) => setChild(e.target.value)}
+                onBlur={() => toggleChildInput(false)}
+              />
+            ) : (
+              <p className="text-[#000000CC] font-normal">{child}</p>
+            )}
+            <Pencil
+              color="#0C21C1B3"
+              size={20}
+              className="cursor-pointer"
+              onClick={() => toggleChildInput(true)}
+            />
           </div>
         </div>
         <div className="flex flex-col border-b-[1px] border-[#00000033] w-[45%] py-4">
           <p className="font-semibold pb-1">How many children do you have</p>
           <div className="flex justify-between">
-            <p className="text-[#000000CC] font-normal">0</p>
-            <Pencil color="#0C21C1B3" size={20} className="cursor-pointer" />
+            {numInput ? (
+              <input
+                value={num}
+                onChange={(e) => setNum(e.target.value)}
+                onBlur={() => toggleNumInput(false)}
+              />
+            ) : (
+              <p className="text-[#000000CC] font-normal">{num}</p>
+            )}
+            <Pencil
+              color="#0C21C1B3"
+              size={20}
+              className="cursor-pointer"
+              onClick={() => toggleNumInput(true)}
+            />
           </div>
         </div>
       </div>
